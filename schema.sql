cd081a6960f86aa85c91e992d95499c78f678747
tarajarellgit <ms.taraj@gmail.com>
2020-08-30 12:23:40 -0700

    after help from Ask BCS

--- schema.sql
+++ schema.sql
@@ -38,7 +38,8 @@ CREATE TABLE dept_emp (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 CREATE TABLE titles (
   emp_no INT NOT NULL,
@@ -46,5 +47,5 @@ CREATE TABLE titles (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, titles, from_date)
 );
