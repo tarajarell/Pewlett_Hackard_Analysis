0d8384998ecfafae6ae06763a2376fb1fbac96bf
tarajarellgit <ms.taraj@gmail.com>
2020-08-30 16:52:44 -0700

    Deliverable 2 done

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,17 +1,34 @@
--- Deliverable 1; Steps 1-5
+-- Create list of retirees by title
 SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
 INTO retirement_titles
 FROM employees AS e
-JOIN titles AS t
+LEFT JOIN titles AS t
 ON e.emp_no = t.emp_no
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+ORDER BY emp_no ASC;
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
 
-INTO nameyourtable
-FROM _______
-ORDER BY _____, _____ DESC;
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+-- Get number of retirees by title
+SELECT COUNT(ut.title), ut.title 
+INTO retiring_titles
+FROM unique_titles AS ut
+GROUP BY ut.title
+ORDER BY count DESC;
+SELECT * FROM retiring_titles;
+-- Create Mentorship Eligibility table
+SELECT DISTINCT ON (emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO mentorship_eligibility
+FROM employees AS e
+LEFT JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+LEFT JOIN titles AS t
+ON e.emp_no = t.emp_no
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31' AND de.to_date = ('9999-01-01'))
+ORDER BY emp_no;
--- schema.sql
+++ schema.sql
@@ -47,5 +47,5 @@ CREATE TABLE titles (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no, titles, from_date)
+	PRIMARY KEY (emp_no, title, from_date)
 );
