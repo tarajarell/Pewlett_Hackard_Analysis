41633ffb8eb268a9597ee6f16534a6291a7d25c3
tarajarellgit <ms.taraj@gmail.com>
2020-08-30 13:54:58 -0700

    second time doing 7.2.2

--- schema.sql
+++ schema.sql
@@ -30,7 +30,7 @@ CREATE TABLE salaries (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no, from_date)
 );
 CREATE TABLE dept_emp (
   emp_no INT NOT NULL,
